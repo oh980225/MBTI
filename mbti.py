fde5b6e8e3bcc9525f2e56bd77bdcb17a1502d17
oh980225 <oh980225@gmail.com>
2022-05-29 22:00:36 +0900

    feat: Ask a question & Predict User's MBTI

--- mbti.py
+++ mbti.py
@@ -5,28 +5,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
+from collections import Counter
 
-MBTI = pd.read_csv('input/MBTI 500.csv')
+def predict(inputList):
+    MBTI = pd.read_csv('input/MBTI 500.csv')
+    input = ""
 
-X = MBTI['posts'] # features
-y = MBTI['type']  # labels
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    for i in inputList:
+        input += i
 
-vectorizer = TfidfVectorizer()
-# Training the vectorizer:
-X_train_tfidf = vectorizer.fit_transform(X_train)
+    X = MBTI['posts'] # features
+    Y = MBTI['type']  # labels
+    vectorizer = TfidfVectorizer()
+    # Training the vectorizer:
+    X_train_tfidf = vectorizer.fit_transform(X)
 
-# Training the classifier:
-clf = LinearSVC()
-clf.fit(X_train_tfidf, y_train)
+    # Training the classifier:
+    clf = LinearSVC()
+    clf.fit(X_train_tfidf, Y)
 
-# Pipelining the vectorizer and the classifier
-text_clf = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC())])
-text_clf.fit(X_train, y_train)
-
-predictions = text_clf.predict(X_test)
-
-print(text_clf.predict(["I need to do an algorithm too. I hate algorithms. I just need to solve a few with the easy ones, hahaha. Oh yeah?? Wow... huh... ah haha ​​there's no director... why do you suddenly come to bed at this hour? I'll wash up. My dog ​​slept deeply while washing his hair, and then he came to dry his hair and left."]))
-print(classification_report(y_test, predictions))
-print(f"Overall accuracy of the model: {round(metrics.accuracy_score(y_test, predictions),2)}")
-
+    # Pipelining the vectorizer and the classifier
+    text_clf = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC())])
+    text_clf.fit(X, Y)
+    return text_clf.predict([input])
